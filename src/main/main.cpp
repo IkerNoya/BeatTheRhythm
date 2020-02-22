5c001c68809e3838fb0b9593b88460144bd294df
IkerNoya <ikernoya4@gmail.com>
2020-02-22 12:44:55 -0300

    Implemented raylib library

---
+++ src/main/main.cpp
@@ -0,0 +1,18 @@
+#include "raylib.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	InitWindow(1280, 720, "BeatTheRhythm v0.1");
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(BLACK);
+		DrawText("PRUEBA", 560, 300, 50, DARKGREEN);
+		EndDrawing();
+	}
+	return 0;
+}
