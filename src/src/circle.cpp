528a39aed204c0b143927f092564a420c0840b73
simon blanco <juansimon@live.com.ar>
2020-02-23 23:54:04 -0300

    Added circle class

---
+++ src/src/circle.cpp
@@ -0,0 +1,31 @@
+#include "circle.h"
+
+void Circle::setRadius(int radius)
+{
+	_radius = radius;
+}
+
+int Circle::getRadius()
+{
+	return _radius;
+}
+
+void Circle::setX(int x)
+{
+	_x = x;
+}
+
+int Circle::getX()
+{
+	return _x;
+}
+
+void Circle::setY(int y)
+{
+	_y = y;
+}
+
+int Circle::getY()
+{
+	return _y;
+}
---
+++ src/src/circle.h
@@ -0,0 +1,26 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+
+class Circle
+{
+	int _radius;
+	int _x;
+	int _y;
+
+public:
+
+	void setRadius(int radius);
+	int getRadius();
+
+	void setX(int x);
+	int getX();
+
+	void setY(int y);
+	int getY();
+};
+#endif // !CIRCLE_H
+
+
+
+
